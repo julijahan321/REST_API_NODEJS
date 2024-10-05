bff2dbfa0e3edf5c841ebdfb8626232f52e7b848
juli-jaan <julijahan.tondra321@gmail.com>
2024-10-05 22:00:43 +0600

    this file is for learn readmefile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,6 +32,7 @@ const createUser = async (req, res) => {
         await newUser.save();
         res.status(201).json(newUser);
     } catch (error) {
+        
         res.status(500).send(error.message); 
     }
 }
